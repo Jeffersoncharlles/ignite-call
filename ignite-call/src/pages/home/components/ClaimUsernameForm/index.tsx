3708435282a18bcbcac438dee2168ba5e78ba91e
jeffersoncharlles <contato@jefferdeveloper.com>
2022-12-23 01:37:35 -0300

    chore:create form and validation using reack-hook-form

--- ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,14 +1,40 @@
-import { Button, TextInput } from "@ignite-ui/react"
+import { Button, Text, TextInput } from "@ignite-ui/react"
 import { ArrowRight } from "phosphor-react"
-import { Form } from "./styles"
+import { useForm } from "react-hook-form"
+import {zodResolver} from '@hookform/resolvers/zod'
+import { z } from "zod"
+import { Form, FormAnnotation } from "./styles"
+
+const ClaimUsernameFormSchema = z.object({
+  username: z.string()
+    .min(3,{message:'O usuário precisa ter 3 letras'})
+    .regex(/^([a-z\\-]+)$/i, {
+       message: 'O usuário apenas pode ter letras e hifens'
+    })//permitir todas letras - 1 ou mais vezes so
+    .transform(username=>username.toLowerCase())
+  ,
+})
+
+type ClaimUsernameForData = z.infer<typeof ClaimUsernameFormSchema>
 
 export const ClaimUsernameForm = () => {
+  const { register, handleSubmit,formState:{errors} } = useForm<ClaimUsernameForData>({
+    resolver: zodResolver(ClaimUsernameFormSchema)
+  })
+
+
+  const handleClaimUsername = async (data: ClaimUsernameForData) => {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
+    <>
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput
         size="sm"
         prefix="jefferdeveloper.com/"
         placeholder="seu-usuário"
+        {...register('username')}
       />
       <Button
         size="sm"
@@ -16,6 +42,13 @@ export const ClaimUsernameForm = () => {
       >
         <ArrowRight />
         Reservar</Button>
-    </Form>
+
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username ? errors.username.message : 'Digite o nome de usuário desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
--- ignite-call/src/pages/home/styles.ts
+++ ignite-call/src/pages/home/styles.ts
@@ -9,6 +9,20 @@ export const Container = styled('div', {
   alignItems: 'center',
   gap: '$20',
   marginLeft: 'auto',
+  position: 'relative',
+
+  '&:before': {
+    content: `''`,
+    display: 'block',
+    backgroundImage: 'url("/Mask-group.svg")',
+    backgroundRepeat:'no-repeat',
+    position: 'absolute',
+    top: '150px',
+    left: '-200px',
+    right: 0,
+    bottom: 0,
+    zIndex: '-1'
+  },
 
   '@media(max-width:600px)': {
     // flexDirection: 'column',
@@ -19,6 +33,8 @@ export const Container = styled('div', {
 export const Hero = styled('div', {
   maxWidth: '480px',
   padding: '0 $10',
+
+
 
   [`> ${Heading}`]: {
     '@media(max-width:600px)': {
