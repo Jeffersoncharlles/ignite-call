a21bd0730608e8338510b5793397c60657817c2f
jeffersoncharlles <contato@jefferdeveloper.com>
2022-12-25 17:09:37 -0300

    chore:create adpater and renpase req and res for adapter

---
+++ ignite-call/src/pages/api/adpater/prisma-adpater.ts
@@ -0,0 +1,239 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { Adapter } from "next-auth/adapters"
+import { destroyCookie, parseCookies } from "nookies"
+import { prisma } from "../../../lib/prisma"
+
+
+export const  PrismaAdapter = (req:NextApiRequest,res:NextApiResponse):Adapter => {
+
+  return {
+//============================================================================//
+    async createUser(user) {
+      //na verdade ele vai fazer um update pegando o id do cookie
+      const { '@ignitecall:userId': userIdOnCookies } = parseCookies({ req })
+
+      if (!userIdOnCookies) {
+        throw new Error('User id not found on cookies.')
+      }
+
+      const prismaUser = await prisma.user.update({
+        where: {
+          id:userIdOnCookies
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url:user.avatar_url,
+        }
+      })
+
+      destroyCookie({ res }, '@ignitecall:userId',{path:'/'})
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,// ! => dizer que isso vai ser informado
+        avatar_url: prismaUser.avatar_url!,
+        emailVerified: null
+      }
+    },
+//============================================================================//
+    async getUser(id) {
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          id
+        }
+      })
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,// ! => dizer que isso vai ser informado
+        avatar_url: user.avatar_url!,
+        emailVerified:null
+      }
+    },
+//============================================================================//
+    async getUserByEmail(email) {
+
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          email
+        }
+      })
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,// ! => dizer que isso vai ser informado
+        avatar_url: user.avatar_url!,
+        emailVerified: null
+      }
+    },
+//============================================================================//
+    async getUserByAccount({ providerAccountId, provider }) {
+
+      const { user } = await prisma.account.findUniqueOrThrow({
+        where: {
+          provider_provider_account_id: {
+            provider,
+            provider_account_id:providerAccountId
+          }
+        },
+        include: {
+          user:true,
+        }//buscar usuário através de outra tabela
+      })
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,// ! => dizer que isso vai ser informado
+        avatar_url: user.avatar_url!,
+        emailVerified: null
+      }
+    },
+//============================================================================//
+    async updateUser(user) {
+
+      const prismaUser = await prisma.user.update({
+        where: {
+         id:user.id!,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url:user.avatar_url
+        }
+      })
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,// ! => dizer que isso vai ser informado
+        avatar_url: prismaUser.avatar_url!,
+        emailVerified: null
+      }
+
+    },
+//============================================================================//
+    async deleteUser(userId) {
+      await prisma.user.delete({
+        where: {
+          id:userId
+        }
+      })
+    },
+//============================================================================//
+    async linkAccount(account) {
+      //logando com outro provider
+      await prisma.account.create({
+        data: {
+          user_id: account.userId,
+          type: account.type,
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+          refresh_token: account.refresh_token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+          scope: account.scope,
+          id_token: account.id_token,
+          session_state:account.session_state
+        }
+      })
+
+    },
+//============================================================================//
+    async unlinkAccount({ providerAccountId, provider }) {
+      await prisma.account.delete({
+        where: {
+          provider_provider_account_id: {
+            provider,
+            provider_account_id:providerAccountId
+          }
+        }
+      })
+
+      return
+    },
+//============================================================================//
+    async createSession({ sessionToken, userId, expires }) {
+      await prisma.session.create({
+        data: {
+          user_id: userId,
+          expires,
+          session_token:sessionToken
+        }
+      })
+
+      return {
+        userId,
+        sessionToken,
+        expires
+      }
+    },
+  //============================================================================//
+    async deleteSession(sessionToken) {
+      await prisma.session.delete({
+        where: {
+          session_token:sessionToken
+        }
+      })
+    },
+//============================================================================//
+    async getSessionAndUser(sessionToken) {
+      const {user,...session} = await prisma.session.findUniqueOrThrow({
+        where: {
+          session_token:sessionToken
+        },
+        include: {
+          user:true
+        }
+      })
+
+
+      return {
+        session: {
+          userId: session.user_id,
+          expires: session.expires,
+          sessionToken:session.session_token
+        },
+        user: {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          email: user.email!,// ! => dizer que isso vai ser informado
+          avatar_url: user.avatar_url!,
+          emailVerified: null
+        }
+      }
+    },
+//============================================================================//
+    async updateSession({ sessionToken,userId,expires }) {
+
+      const prismaSession = await prisma.session.update({
+        where: {
+          session_token:sessionToken
+        },
+        data: {
+          expires,
+          user_id:userId
+        }
+      })
+
+      return {
+        sessionToken: prismaSession.session_token,
+        userId: prismaSession.user_id,
+        expires: prismaSession.expires
+      }
+
+    },
+//============================================================================//
+  }
+}
--- ignite-call/src/pages/api/auth/[...nextauth].api.ts
+++ ignite-call/src/pages/api/auth/[...nextauth].api.ts
@@ -1,30 +1,41 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
+import type { NextApiRequest, NextApiResponse } from "next"
 import GoogleProvider from "next-auth/providers/google"
-export const authOptions:NextAuthOptions = {
-  // Configure one
+import { PrismaAdapter } from "../adpater/prisma-adpater"
+
+export const buildNextAuthOptions = (req:NextApiRequest,res:NextApiResponse): NextAuthOptions => {
 
 
+  return {
+    // Configure one
+    adapter: PrismaAdapter(req,res),//chamando os adapters
 
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          scope:'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            scope: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
+          }
+        },
+      }),
+      // ...add more providers here
+    ],
+    callbacks: {
+      async signIn({ account }) {
+        if (!account?.scope?.includes('https://www.googleapis.com/auth/calendar')) {
+          return '/register/connect-calendar?error=permissions'
+          //false so que redirection
         }
-      },
-    }),
-    // ...add more providers here
-  ],
-  callbacks: {
-    async signIn({account}) {
-      if (!account?.scope?.includes('https://www.googleapis.com/auth/calendar')) {
-        return '/register/connect-calendar?error=permissions'
-        //false so que redirection
+        return true
       }
-      return true
-   }
+    }
   }
 }
-export default NextAuth(authOptions)
+
+
+export default async function auth (req: NextApiRequest, res: NextApiResponse) {
+
+  return await NextAuth(req, res, buildNextAuthOptions(req,res))
+}
