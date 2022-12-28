148b025e53b29d2b85df1cef8de91042b5fffcd4
jeffersoncharlles <contato@jefferdeveloper.com>
2022-12-28 13:29:23 -0300

    chore:TimerPicker styles

--- ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,13 +1,45 @@
 import { Calendar } from "../../../../../components/Calendar";
-import { Container } from "./styles";
+import { Container, TimePicker, TimePickerHeader, TimePickerList, TimerPickerItem } from "./styles";
 
 
 
 
 export function CalendarStep() {
+
+  const hasSelectedDate = true
+
+
   return (
-    <Container>
+    <Container isTimePickerOpen={hasSelectedDate}>
       <Calendar />
+
+      {hasSelectedDate && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimerPickerItem>08:00h</TimerPickerItem>
+            <TimerPickerItem>09:00h</TimerPickerItem>
+            <TimerPickerItem>10:00h</TimerPickerItem>
+            <TimerPickerItem>11:00h</TimerPickerItem>
+            <TimerPickerItem>12:00h</TimerPickerItem>
+            <TimerPickerItem>13:00h</TimerPickerItem>
+            <TimerPickerItem>14:00h</TimerPickerItem>
+            <TimerPickerItem>15:00h</TimerPickerItem>
+            <TimerPickerItem>16:00h</TimerPickerItem>
+            <TimerPickerItem>17:00h</TimerPickerItem>
+            <TimerPickerItem>18:00h</TimerPickerItem>
+            <TimerPickerItem>19:00h</TimerPickerItem>
+            <TimerPickerItem>20:00h</TimerPickerItem>
+            <TimerPickerItem>21:00h</TimerPickerItem>
+            <TimerPickerItem>22:00h</TimerPickerItem>
+            <TimerPickerItem>23:00h</TimerPickerItem>
+            <TimerPickerItem>24:00h</TimerPickerItem>
+            <TimerPickerItem>01:00h</TimerPickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
    </Container>
   )
 }
